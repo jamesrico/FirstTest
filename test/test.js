2d8096844297c22da1f8fde46ecf774eeaf64a9b
James Rico <jamesrico089@gmail.com>
2015-01-23 14:26:07 -0600

    third test

--- test/test.js
+++ test/test.js
@@ -1,13 +1,28 @@
 /* global describe, it */
 /* jshint expr: true */
+
 'use strict';
 
-var expect = require('chai').expect;
+var expect      = require('chai').expect,
+    Constructor = require(process.cwd() + '/lib/Constructor');
 
 describe('Mocha', function () {
   it('should expect true and false', function () {
     expect(true).to.be.true;
     expect(false).to.be.false;
+  });
+});
+
+describe('Constructor', function () {
+  describe('init', function () {
+    it('should have public variables', function () {
+      var constructed = new Constructor();
+      expect(constructed.publicVariable).to.equal('foo');
+    });
+    it('should accept an argument', function () {
+      var constructed = new Constructor('bar');
+      expect(constructed.publicVariable).to.equal('bar');
+    });  
   });
 });
 
