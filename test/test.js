33e5da372a15b7fc368d0e7a2c49611d5ea3c4f3
James Rico <jamesrico089@gmail.com>
2015-01-23 15:19:39 -0600

    more test

--- test/test.js
+++ test/test.js
@@ -20,10 +20,19 @@ describe('Constructor', function () {
       expect(constructed.publicVariable).to.equal('foo');
     });
     it('should accept an argument', function () {
-      var constructed = new Constructor('bar');
-      expect(constructed.publicVariable).to.equal('bar');
-    });  
+      var barConstructed = new Constructor('bar'),
+          bazConstructed = new Constructor('baz');
+
+      expect(barConstructed.publicVariable).to.equal('bar');
+      expect(bazConstructed.publicVariable).to.equal('baz');
+    });
+  });
+  describe('#publicMethod', function () {
+    it('should return "Hello World"', function () {
+      var constructed = new Constructor();
+      expect(constructed.publicMethod()).to.equal('Hello World');
+      expect(constructed.hasOwnProperty('publicMethod')).to.be.false
+    });
   });
 });
-
 
